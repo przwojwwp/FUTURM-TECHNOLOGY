7f99e546e5183145147897696635be5f4873add5
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-12-03 19:44:24 +0100

    feat: create CamnpaignService to manage campaign data

--- src/app/campaign.service.ts
+++ src/app/campaign.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Campaign } from './models/campaign.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CampaignService {
-
-  constructor() { }
+  private campaigns: Campaign[] = [
+    new Campaign('Black Friday Sale', ['Electronics', 'Discount'], 100, 500, true, 'New York', 10),
+    new Campaign('Christmas Sale', ['Toys', 'Gift'], 150, 300, false, 'Los Angeles', 20),
+  ];
 }
