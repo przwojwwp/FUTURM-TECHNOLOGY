53bd581c007bd0aa2c91942bbd10496a7ec0a584
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-12-03 19:46:25 +0100

    feat: add CRUD methods to manage campaigns

--- src/app/campaign.service.ts
+++ src/app/campaign.service.ts
@@ -9,4 +9,20 @@ export class CampaignService {
     new Campaign('Black Friday Sale', ['Electronics', 'Discount'], 100, 500, true, 'New York', 10),
     new Campaign('Christmas Sale', ['Toys', 'Gift'], 150, 300, false, 'Los Angeles', 20),
   ];
+
+  getCampaigns() {
+    return [...this.campaigns];
+  }
+
+  createCampaign(campaign: Campaign) {
+    this.campaigns.push(campaign);
+  }
+
+  updateCampaign(index: number, updatedCampaign: Campaign) {
+    this.campaigns[index] = updatedCampaign;
+  }
+
+  deleteCampaign(index: number) {
+    this.campaigns.splice(index, 1);
+  }
 }
